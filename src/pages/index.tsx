f183a29ceabe4f53e13a9e762138c5e98fc060a7
Thomas Cardon <thomas.cardon@pm.me>
2021-11-27 15:52:59 +0100

    Ajout de Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,15 @@ export default () => {
     <Page>
       <Section>
         <h2>Home</h2>
+
+        <div className="m-1 pt-4 text-center">
+          <div className="p-2">
+            <div className="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-yellow text-sm">
+              <span className="inline-flex bg-yellow-400 text-white rounded-full h-6 px-3 justify-center items-center">Information</span>
+              <span className="inline-flex text-xs sm:text-base px-2">Changez votre année/groupe dans les paramètres si nécessaire.</span>
+            </div>
+          </div>
+        </div>
       </Section>
 
       <FakeList />
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,12 +1,31 @@
 import Page from '../components/page'
 import Section from '../components/section'
+import { Bell, BellOff } from 'react-feather'
+import { useState } from 'react'
 
-const Settings = () => (
-    <Page title='Paramètres'>
-        <Section>
-            <h2>Paramètres</h2>
-        </Section>
-    </Page>
-)
+import { isMobileSafari } from 'react-device-detect';
+
+const Settings = () => {
+    const [notifications, setNotifications] = useState(false)
+
+    return (
+        <Page title='Paramètres'>
+            <Section>
+                <h2 className="text-3xl">Paramètres</h2>
+                <p className="text-sm text-gray-200">Ces paramètres seront enregistrés sur votre appareil.</p>
+
+                <Section>
+                    <h3>Notifications</h3>
+                    <p className="text-xs text-gray-200">Recevez des notifications de la part de l'IUT en cas d'informations urgentes.</p>
+                    <button disabled={isMobileSafari} onClick={() => setNotifications(!notifications)} className="flex items-center justify-center px-2 py-2 my-3 text-sm bg-blue-600 space-x-3 rounded-lg lg:w-auto focus:outline-none">
+                        {notifications ? <BellOff size={16} /> : <Bell size={16} />}
+                        <span>{notifications ? 'Désactiver' : 'Activer'}</span>
+                    </button>
+                </Section>
+            </Section>
+
+        </Page>
+        )
+    }
 
 export default Settings
