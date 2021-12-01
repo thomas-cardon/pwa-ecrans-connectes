81122252961a51675218e989de09865395d62728
Thomas Cardon <thomas.cardon@pm.me>
2021-12-01 19:00:54 +0100

    Titre: utilisation react feather au lieu de SVGs

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,4 +1,5 @@
 import Section from './section'
+import { Share, Edit2 } from 'react-feather'
 
 type Props = {
   showButtons: boolean,
@@ -19,18 +20,14 @@ const Title = ({ showButtons, subtitle, children }) => (
         <div className="flex mt-5 lg:mt-0 lg:ml-4">
           <span className="hidden sm:block">
             <button disabled type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm disabled:opacity-50 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <svg className="w-5 h-5 mr-2 -ml-1 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-              </svg>
+              <Edit2 />&nbsp;
               Modifier
             </button>
           </span>
 
           <span className="hidden ml-3 sm:block">
             <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <svg className="w-5 h-5 mr-2 -ml-1 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" />
-              </svg>
+              <Share />&nbsp;
               Partager
             </button>
           </span>
