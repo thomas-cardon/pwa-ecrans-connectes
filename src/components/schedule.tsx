967cb6c667cbfd2fc9d615c4a81a6c79f6ee435d
Thomas Cardon <thomas.cardon@pm.me>
2021-12-01 19:01:31 +0100

    L'emploi du temps utilise désormais les données issues de l'API

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -14,85 +14,83 @@ function stringToColor(str = 'xxx') {
     return colour;
 }
 
-const Schedule = () => {
+
+type Props = {
+    data: Array<any>
+}
+
+type CourseProps = {
+    summary: string,
+    location: string,
+    description: string,
+    dtstart: string,
+    dtend: string
+}
+
+function process(course: CourseProps) {
+    let obj = {
+        label: course.summary,
+        description: course.description,
+        location: course.location,
+        start: new Date(course.dtstart.replace(/(....)(..)(.....)(..)(.*)/, '$1-$2-$3:$4:$5')),
+        end: new Date(course.dtend.replace(/(....)(..)(.....)(..)(.*)/, '$1-$2-$3:$4:$5')),
+    };
+    
+    return obj;
+}
+
+const Schedule = ({ data } : Props) => {
     const [code, setCode] = useLocalStorage("schedule.adeCode", 8402);
     const [schedule, setSchedule] = useLocalStorage("schedule.data", []);
 
     let date = new Date();
 
-    const data = [
-        {
-            "label": "M3203 Droit des TIC TD G2",
-            "description": "1605013683654\nGroupe 2 an2\nBERTHET Anne Charlotte\n\n(Modifié le:08/11/2021 16:46)",
-            "location": "TD 1 - 2",
-            "deb": "20211130T071500Z",
-            "fin": "20211130T091500Z",
-            "start": "6h15",
-            "end": "8h15"
-        },
-        {
-            "label": "M3204 Gestion des SI TD G2*",
-            "description": "Groupe 2 an2\nMAKSSOUD Christine\n\n(Modifié le:16/11/2021 13:26)",
-            "location": "E 002",
-            "deb": "20211130T091500Z",
-            "fin": "20211130T111500Z",
-            "start": "8h15",
-            "end": "10h15"
-        },
-        {
-            "label": "M3206 Anglais TD G2",
-            "description": "Groupe 2 an2\nSLEZAK Eileen\n\n(Modifié le:22/09/2021 12:41)",
-            "location": "Laboratoire Multimédia langues 108",
-            "deb": "20211130T123000Z",
-            "fin": "20211130T143000Z",
-            "start": "11h30",
-            "end": "13h30"
-        },
-        {
-            "label": "M3303 PPP TD G2",
-            "description": "Groupe 2 an2\nTANTI Marc\n\n(Modifié le:22/09/2021 12:41)",
-            "location": "TD 1 - 2",
-            "deb": "20211130T143000Z",
-            "fin": "20211130T163000Z",
-            "start": "13h30",
-            "end": "15h30"
+    Object.keys(data).forEach(key => data[key] = process(data[key]));
+
+    let dataSortedByDate = {};
+    data.forEach(course => {
+        let date = course.start.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        if (!dataSortedByDate[date]) {
+            dataSortedByDate[date] = [];
         }
-    ];
+        dataSortedByDate[date].push(course);
+    });
+    
+    return <>
+        {Object.entries(dataSortedByDate).map(([date, courses]: [string, Array<any>], i) => (<div key={i}>
+                <div className="p-6 overflow-hidden rounded-xl bg-gradient-to-r from-yellow-50 to-yellow-100 sm:p-10">
+                    <p className="text-xl not-italic text-center text-yellow-800 sm:text-3xl">{date}</p>
+                </div>
+                {courses.map((course, j) =>
+                    <div className="py-6" key={i + '-' + j}>
+                        <div className="p-4 shadow-xl sm:p-6 dark:bg-gray-800 rounded-xl">
+                            <h2 className="mt-6 text-3xl font-bold text-gray-800 dark:text-white">{course.label.replace(/\*/g, '')}</h2>
+                            <p className="text-gray-500 whitespace-pre-line text-md dark:text-gray-50">{course.description.replace(/\([^()]*\)/g, '').replace(/\([^()]*\)/g, '').replace(/[0-9]{10,}/, '').trim()}</p>
+                            <p className="text-gray-500 whitespace-pre-line text-md dark:text-gray-50">
+                                <i>📍 {course.location}</i>
+                            </p>
 
-    return (<div>
-            <div className="p-6 overflow-hidden rounded-xl bg-gradient-to-r from-yellow-50 to-yellow-100 sm:p-10">
-                <p className="text-xl not-italic text-center text-yellow-800 sm:text-3xl">{date.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
-            </div>
-            {data.map(course => (
-                <div className="py-6" key={course.deb}>
-                    <div className="p-4 shadow-xl sm:p-6 dark:bg-gray-800 rounded-xl">
-                        <h2 className="mt-6 text-3xl font-bold text-gray-800 dark:text-white">{course.label}</h2>
-                        <p className="text-gray-500 whitespace-pre-line text-md dark:text-gray-50">{course.description.replace(/\([^()]*\)/g, '').replace(/\([^()]*\)/g, '').replace(/[0-9]{10,}/, '').trim().replace('*', '')}</p>
-                        <p className="text-gray-500 whitespace-pre-line text-md dark:text-gray-50">
-                            <i>📍 {course.location}</i>
-                        </p>
+                            <div className="flex flex-col items-center justify-center">
 
-                        <div className="flex flex-col items-center justify-center">
-
-                            <div className="relative w-full h-32 p-4 mt-4 bg-center bg-cover rounded-lg shadow-xl bg-gradient-to-r from-indigo-50 to-indigo-100 dark:from-gray-300 dark:to-gray-400">
-                                <div className="absolute inset-0 bg-opacity-50 rounded-lg backdrop-filter backdrop-blur-md"></div>
-                                <div className="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
-                                    <div>
-                                        <h3 className="text-lg text-center text-gray-700 dark:text-white">
-                                            Ce cours est de:
-                                        </h3>
-                                        <h3 className="mt-2 text-3xl font-bold text-center text-gray-700 dark:text-white">
-                                            {course.start} à {course.end}
-                                        </h3>
+                                <div className="relative w-full h-32 p-4 mt-4 bg-center bg-cover rounded-lg shadow-xl bg-gradient-to-r from-indigo-50 to-indigo-100 dark:from-gray-300 dark:to-gray-400">
+                                    <div className="absolute inset-0 bg-opacity-50 rounded-lg backdrop-filter backdrop-blur-md"></div>
+                                    <div className="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
+                                        <div>
+                                            <h3 className="text-lg text-center text-gray-700 dark:text-white">
+                                                Ce cours est de:
+                                            </h3>
+                                            <h3 className="mt-2 text-3xl font-bold text-center text-gray-700 dark:text-white">
+                                                {course.start.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' })} à {course.end.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' })}
+                                            </h3>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    )
-}
+                )}
+            </div>))}
+        </>
+    }
 
 export default Schedule
--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,13 +1,23 @@
+import useSWR from 'swr';
+
 import Page from '../components/page'
 import Section from '../components/section'
 import Schedule from '../components/schedule'
 
-const SchedulePage = () => (
-  <Page title='Emploi du temps' showButtons={true}>
-    <Section>
-      <Schedule />
-    </Section>
-  </Page>
-)
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const SchedulePage = () => {
+  const { data, error } = useSWR(`${process.env.ecranConnectesAddress}/v1/schedule`, fetcher)
+
+  if (error) console.error(error);
+  return (
+    <Page title='Emploi du temps' showButtons={true}>
+      <Section>
+        {error && <pre><code>{error}</code></pre>}
+        {data && <Schedule data={data?.data || []} />}
+      </Section>
+    </Page>
+  )
+}
 
 export default SchedulePage
