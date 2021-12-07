550a71989cb6bd10369c6e8342ac35064d1c94ae
Thomas Cardon <thomas.cardon@pm.me>
2021-12-07 15:22:11 +0100

    Removed env variables

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  env: {
-    title: 'Dpt. Info',
-    brand: null, // optional, URL to logo image
-    ecranConnectesAddress: 'http://localhost/tv/wp-json/amu-ecran-connectee', // Adresse du serveur de l'écran connecté, communiquant avec la PWA
-    NOTIFICATIONS_API_KEY: 'a054e57e-15bd-48c0-b9ec-858210066e27'
-  },
   pwa: {
     dest: 'public',
     runtimeCaching,
